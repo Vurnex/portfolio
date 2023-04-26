4476a90a0f16768107675767e817eb81c9a546da
Vurnex <hamilton.laquon@gmail.com>
2023-04-26 16:10:05 -0500

    Add logic for handling form

--- js/script.js
+++ js/script.js
@@ -41,3 +41,32 @@ if (currentTheme) {
         document.getElementById("checkbox2").checked = true;
     }
 }
+
+//Handle form
+const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+    .then(function(data) { 
+        
+      formAlertFail.style.display = "none";
+      formAlertSuccess.style.display = "block"; })
+  
+    .catch(function(error) {  
+        
+      formAlertSuccess.style.display = "none";
+      formAlertFail.style.display = "block"; 
+    });
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const formAlertSuccess = document.getElementById("form-alert-success");
+const formAlertFail = document.getElementById("form-alert-fail");
